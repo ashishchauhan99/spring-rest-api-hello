b0bc894c36f72a3c611d83f8ed707ba4ba0e0be8
Kumar <ashish.kumar31@sap.com>
2024-09-27 17:03:20 +0200

    first commit

---
+++ src/main/java/net/codejava/Application.java
@@ -0,0 +1,20 @@
+package net.codejava;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Application {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) throws Exception {
+         SpringApplication.run(Application.class, args);
+    }
+
+
+}
